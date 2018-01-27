87c40cc02269af9c2d8a7293bf99f3579e19c51b
LordSpoon <haakon.mykle_bust@hotmail.com>
2018-01-27 17:10:05 +0100

    ads

---
+++ Transmission/Assets/Scripts/Npc's/NpcBrain.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcBrain : MonoBehaviour {
+
+
+    public string[] textOptions;
+    public string[] playerTextOptions;
+    public int numberOfTextOptions;
+    public int numberOfPlayerTextOptions;
+
+
+    private void Start()
+    {
+        numberOfTextOptions = textOptions.Length;
+        numberOfPlayerTextOptions = playerTextOptions.Length;
+    }
+
+}
