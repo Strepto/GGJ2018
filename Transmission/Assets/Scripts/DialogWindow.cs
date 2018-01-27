f4bd0dbaa0b406bf52614fafcf81ba334bbbcdc5
LordSpoon <haakon.mykle_bust@hotmail.com>
2018-01-27 16:43:18 +0100

    sdgf

--- Transmission/Assets/Scripts/DialogWindow.cs
+++ Transmission/Assets/Scripts/DialogWindow.cs
@@ -13,41 +13,66 @@ public class DialogWindow : Singleton<DialogWindow> {
 
     public Sprite[] playerPortraits;
 
+    public GameObject npcTextPrefab;
+    public GameObject playerTextPrefab;
+    public GameObject textPanel;
+
+    private bool hasStartedDialog;
+
     void Start () {
         ActiveDialog = false;
+        hasStartedDialog = false;
     }
 	
 	void Update () {
-		
+		if (ActiveDialog && !hasStartedDialog)
+        {
+            hasStartedDialog = true;
+            initiateDialog();
+        }
 	}
 
     public void startDialog(DialogHandler handler)
     {
         currentHandler = handler;
         ActiveDialog = true;
+    }
 
+    private void initiateDialog()
+    {
         if (player.CurrentPlayerState == PlayerController.PlayerState.Boy)
         {
             playerImage.sprite = playerPortraits[0];
+            writeNpcMessage("Hi boy");
         }
         else
         {
             playerImage.sprite = playerPortraits[1];
+            writeNpcMessage("Hi girl");
         }
+    }
+
+    public void sendReply(int replyNr)
+    {
+
     }
 
     public void endDialog()
     {
         ActiveDialog = false;
+        hasStartedDialog = false;
+        currentHandler.EndDialog();
     }
 
     public void writeNpcMessage(string text)
     {
-
+        GameObject newTextBubble = Instantiate(npcTextPrefab, textPanel.transform);
+        newTextBubble.transform.Find("Panel").transform.Find("Text").GetComponent<Text>().text = text;
     }
 
     public void writePlayerMessage(string text)
     {
-
+        GameObject newTextBubble = Instantiate(playerTextPrefab, textPanel.transform);
+        newTextBubble.transform.Find("Panel").transform.Find("Text").GetComponent<Text>().text = text;
     }
 }
