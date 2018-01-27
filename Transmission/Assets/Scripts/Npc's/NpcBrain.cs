68658560f2789a5d0b4d0a2b05c1ae55c7f949bf
LordSpoon <haakon.mykle_bust@hotmail.com>
2018-01-27 21:20:52 +0100

    sdf

--- Transmission/Assets/Scripts/Npc's/NpcBrain.cs
+++ Transmission/Assets/Scripts/Npc's/NpcBrain.cs
@@ -4,15 +4,83 @@ using Transmission;
 using UnityEngine;
 
 public class NpcBrain : MonoBehaviour {
-
-    public DialogHandler dialog;
-
+    
     protected int stateNumber;
 
-    private void Start()
+    public MovementHandler movement;
+    public DialogHandler dialog;
+
+    public Vector2[] pointList;
+    protected int pointInList = 0;
+    public float speed;
+
+    void Start()
     {
+        MoveToPoint(pointList[pointInList]);
         stateNumber = 0;
     }
+
+    void Update()
+    {
+
+    }
+
+    public void PlayerInitiatedDialog()
+    {
+        startsDialog();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            startsDialog();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            MoveToPoint(pointList[pointInList]);
+        }
+    }
+
+    private void startsDialog()
+    {
+        Stop();
+        dialog.startDialog();
+    }
+
+    void Stop()
+    {
+        movement.Stop();
+    }
+
+    void MoveToPoint(Vector2 point)
+    {
+        movement.MoveToPosition(point, speed, callback: HasMovedToPoint);
+    }
+
+    void HasMovedToPoint(bool interrupted, float timeUsed)
+    {
+        if (!interrupted)
+        {
+            pointInList++;
+            if (pointInList >= pointList.Length)
+            {
+                pointInList = 0;
+            }
+
+            MoveToPoint(pointList[pointInList]);
+        }
+
+    }
+
+
+    
+
+
 
     public virtual string getInitialText()
     {
