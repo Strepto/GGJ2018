ab9b1860a05bea3d81395a217e0277d9557c1fce
LordSpoon <haakon.mykle_bust@hotmail.com>
2018-01-28 15:18:08 +0100

    sfd

--- Transmission/Assets/Scripts/Npc's/BartenderScript.cs
+++ Transmission/Assets/Scripts/Npc's/BartenderScript.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class BartenderScript : NpcBrain
 {
     public PickupItem money;
+    public PickupItem vodka;
+    public PickupItem cosmopolitan;
 
     private bool hasGivenQuest = false;
 
@@ -26,7 +28,7 @@ public class BartenderScript : NpcBrain
             return "Have you saved him yet?";
         }
         hasGivenQuest = true;
-        return "A friend of mine is being bullied, can you save him for me?";
+        return "What can I do for you?";
     }
 
     public override string getTextAndSendReply(int choice)
@@ -34,19 +36,89 @@ public class BartenderScript : NpcBrain
         switch (state)
         {
             case "initialState":
-                return "endDialog()";
-            case "hasPurse":
+                if (choice == 1)
+                {
+                    state = "buyDrink";
+                    return "Of course, what do you want?";
+                }
+                if (choice == 2)
+                {
+                    return "endDialog()";
+                }
+                else
+                {
+                    state = "gossip";
+                    if (hasGivenQuest)
+                    {
+                        return "My poor friend...";
+                    }
+                    return "A friend of mine is being bullied. He's having a really hard time...";
+                }
+            case "hasSavedVictim":
                 if (choice == 2)
                 {
                     state = "thanks";
                     PickupItem newMoney = Instantiate(money);
-                    //newMoney.Amount = 500;
+                    newMoney.SetAmount(500);
                     PlayerController.Instance.ItemGiveToPlayer(Instantiate(money));
                     return "Thank you! Take this as a reward!";
+                }
+                if (choice == 1)
+                {
+                    state = "buyDrink";
+                    return "Of course, what do you want?";
+                }
+                else
+                {
+                    state = "gossip";
+                    return "A friend of mine is being bullied. He's having a really hard time...";
+                }
+            case "gossip":
+                if (choice == 0)
+                {
+                    hasGivenQuest = true;
+                    return "Thank you! If you can help him, I'll give you a reward!";
                 }
                 else
                 {
                     return "endDialog()";
+                }
+            case "buyDrink":
+                if (choice == 0)
+                {
+                    return "endDialog()";
+                }
+                if (choice == 1)
+                {
+                    if (PlayerController.Instance.ItemCheck("money") >= 5)
+                    {
+                        PlayerController.Instance.ItemTake("money", 5);
+                        PlayerController.Instance.ItemGiveToPlayer(cosmopolitan);
+                        state = "hasBoughtDrink";
+                        return "Here you go! Anything else?";
+                    }
+                    state = "initialState";
+                    if (PlayerController.Instance.CurrentPlayerState == PlayerController.PlayerState.Boy)
+                    {
+                        return "You don't have enough cash sonny!";
+                    }
+                    return "You don't have enough cash lassy!";
+                }
+                else
+                {
+                    if (PlayerController.Instance.ItemCheck("money") >= 10)
+                    {
+                        PlayerController.Instance.ItemTake("money", 10);
+                        PlayerController.Instance.ItemGiveToPlayer(cosmopolitan);
+                        state = "hasBoughtDrink";
+                        return "Here you go! Anything else?";
+                    }
+                    state = "initialState";
+                    if (PlayerController.Instance.CurrentPlayerState == PlayerController.PlayerState.Boy)
+                    {
+                        return "You don't have enough cash sonny!";
+                    }
+                    return "You don't have enough cash lassy!";
                 }
             case "thanks":
                 return "endDialog()";
@@ -63,23 +135,23 @@ public class BartenderScript : NpcBrain
                 switch (choiceNr)
                 {
                     case 0:
-                        return "Sure thing!";
+                        return "Got any gossip?";
                     case 1:
-                        return "Why should I care?";
+                        return "Can I have a drink?";
                     case 2:
-                        return "";
+                        return "Never mind...";
                     default:
                         return "error";
                 }
-            case "hasPurse":
+            case "hasSavedVictim":
                 switch (choiceNr)
                 {
                     case 0:
-                        return "It's mine now!";
+                        return "Got any gossip?";
                     case 1:
-                        return "Why should I care?";
+                        return "Can I have a drink?";
                     case 2:
-                        return "Here you go!";
+                        return "Your friend has been saved!";
                     default:
                         return "error";
                 }
@@ -89,7 +161,44 @@ public class BartenderScript : NpcBrain
                     case 0:
                         return "You're welcome!";
                     case 1:
-                        return "I should've kept it...";
+                        return "Thank you!";
+                    case 2:
+                        return "";
+                    default:
+                        return "error";
+                }
+            case "buyDrink":
+                switch (choiceNr)
+                {
+                    case 0:
+                        return "I've changed my mind.";
+                    case 1:
+                        return "I want some vodka (5$)";
+                    case 2:
+                        return "Give me a cosmopolitan! (10$)";
+                    default:
+                        return "error";
+                }
+            case "hasBoughtDrink":
+                state = "buyDrink";
+                switch (choiceNr)
+                {
+                    case 0:
+                        return "No, that's all";
+                    case 1:
+                        return "I want some vodka (5$)";
+                    case 2:
+                        return "Give me a cosmopolitan! (10$)";
+                    default:
+                        return "error";
+                }
+            case "gossip":
+                switch (choiceNr)
+                {
+                    case 0:
+                        return "Maybe I can help!";
+                    case 1:
+                        return "That's too bad...";
                     case 2:
                         return "";
                     default:
